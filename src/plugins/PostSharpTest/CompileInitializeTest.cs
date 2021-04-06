c5507ac67b33283bb88cd9bd7a8c363fceacd1a7
jerviscui <cuijervis@126.com>
2021-04-06 12:02:14 +0800

    Initialize Test

---
+++ src/plugins/PostSharpTest/CompileInitializeTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using PostSharp.Aspects;
+using PostSharp.Extensibility;
+using PostSharp.Serialization;
+
+namespace PostSharpTest
+{
+    [Serializable]
+    public class CompileInitializeAttribute: OnMethodBoundaryAspect
+    {
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        ///   Method executed <b>before</b> the body of methods to which this aspect is applied.
+        /// </summary>
+        /// <param name="args">Event arguments specifying which method
+        /// is being executed, which are its arguments, and how should the execution continue
+        /// after the execution of <see cref="M:PostSharp.Aspects.IOnMethodBoundaryAspect.OnEntry(PostSharp.Aspects.MethodExecutionArgs)" />.</param>
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            Console.WriteLine($"exec time: {DateTime:O}");
+        }
+
+        /// <summary>
+        ///   Method invoked at build time to initialize the instance fields of the current aspect. This method is invoked
+        ///   before any other build-time method.
+        /// </summary>
+        /// <param name="method">Method to which the current aspect is applied</param>
+        /// <param name="aspectInfo">Reserved for future usage.</param>
+        public override void CompileTimeInitialize(MethodBase method, AspectInfo aspectInfo)
+        {
+            DateTime = DateTime.Now;  
+        }
+    }
+
+    [CompileInitialize(AttributeTargetElements = MulticastTargets.Method)]
+    public class CompileInitializeTest
+    {
+        public CompileInitializeTest Test()
+        {
+            Console.WriteLine("DateTime is Compile time.");
+
+            return this;
+        }
+    }
+}
--- src/plugins/PostSharpTest/Program.cs
+++ src/plugins/PostSharpTest/Program.cs
@@ -8,26 +8,34 @@ namespace PostSharpTest
     {
         static async Task Main(string[] args)
         {
+            //1
             //new ServiceC().Do();
             //Console.WriteLine();
             //new ServiceD().Do();
 
+            //2
             //var ser = new LogTestService();
             //await ser.DoAsync();
             //ser.Dispose();
 
+            //3
             //new OnExceptionAspectTest().Test();
 
+            //4
             //var t1 = new OnMethodBoundaryAspectTest();
             //t1.Test1();
             //t1.Test2();
             //new OnMethodBoundaryAspectTest().Test2();
 
-            new InstanceScopedTest().Test();
-            new InstanceScopedTest().Test();
+            //5
+            //new InstanceScopedTest().Test();
+            //new InstanceScopedTest().Test();
 
-            new InstanceSingletonTest().Test();
-            new InstanceSingletonTest().Test();
+            //new InstanceSingletonTest().Test();
+            //new InstanceSingletonTest().Test();
+
+            //6
+            new CompileInitializeTest().Test();
 
             Console.WriteLine("Hello World!");
         }
