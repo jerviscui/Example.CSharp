0e2f4a786a991fd2257fd4d4929a970b83c3ebc1
jerviscui <cuijervis@126.com>
2021-04-16 00:11:31 +0800

    add Lazy.Fody test

---
+++ src/plugins/Fody/LazyTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using Lazy;
+
+namespace LazyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var t = new Test();
+            Console.WriteLine(t.StrService);
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine(t.StrService);
+        }
+    }
+
+    public class Test
+    {
+        [Lazy]
+        public string StrService => $"{DateTime.Now:O}";
+    }
+}
