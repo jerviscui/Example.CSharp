6043c08a59eb963de1d1321a7672d01accee2235
jerviscui <cuijervis@126.com>
2021-04-22 23:18:40 +0800

    add BenchmarkTests

---
+++ src/9/ForeachTest/BenchmarkTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace ForeachTest
+{
+    [MemoryDiagnoser]
+    public class ToArrayAndListTests
+    {
+        [Params(5_000, 5_000_000)]
+        public int Count { get; set; }
+
+        public IEnumerable<int> Items => Enumerable.Range(0, Count);
+
+        [Benchmark(Description = "ToArray()", Baseline = true)]
+        public int[] ToArray() => Items.ToArray();
+
+        [Benchmark(Description = "ToList()")]
+        public List<int> ToList() => Items.ToList();
+    }
+
+    [MemoryDiagnoser]
+    public class ForeachTest
+    {
+        public IEnumerable<int> IEnumerable = Enumerable.Range(0, 5_000_000).ToArray();
+
+        public int[] Array = Enumerable.Range(0, 5_000_000).ToArray();
+
+        public IList<int> IList = Enumerable.Range(0, 5_000_000).ToArray();
+
+        public List<int> List = Enumerable.Range(0, 5_000_000).ToArray().ToList();
+
+        [Benchmark(Description = "ArrayForeachTest", Baseline = true)]
+        public void ArrayForeachTest()
+        {
+            foreach (var item in Array)
+            {
+                var a = item + 1;
+            }
+        }
+
+        [Benchmark(Description = "IEnumerableForeachTest")]
+        public void IEnumerableForeachTest()
+        {
+            foreach (var item in IEnumerable)
+            {
+                var a = item + 1;
+            }
+        }
+
+        [Benchmark(Description = "IListForeachTest")]
+        public void IListForeachTest()
+        {
+            foreach (var item in IList)
+            {
+                var a = item + 1;
+            }
+        }
+
+        [Benchmark(Description = "ListForeachTest")]
+        public void ListForeachTest()
+        {
+            foreach (var item in List)
+            {
+                var a = item + 1;
+            }
+        }
+    }
+}
--- src/9/ForeachTest/Program.cs
+++ src/9/ForeachTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using BenchmarkDotNet.Running;
 
 namespace ForeachTest
 {
@@ -9,47 +10,111 @@ namespace ForeachTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ToArray 快
+            BenchmarkRunner.Run<ToArrayAndListTests>();
 
+            //array 最快，IEnumerable IList 有装箱，
+            BenchmarkRunner.Run<ForeachTest>();
+            
+            //Test();
+        }
+
+        public static void Test()
+        {
             IEnumerableForeachTest();
             IListForeachTest();
             ListForeachTest();
 
-            //list dictionary array set queue
+            // array 最快，IEnumerable IList 有装箱，
+            //i=0,时间:1
+            //i=1,时间:1
+            //i=2,时间:1
+            //i=3,时间:1
+            //i=4,时间:1
+            //i=5,时间:1
+            //i=6,时间:1
+            //i=7,时间:1
+            //i=8,时间:1
+            //i=9,时间:1
+            //avg:1
+            //IEnumerable---------------
+            //i=0,时间:20
+            //i=1,时间:20
+            //i=2,时间:20
+            //i=3,时间:20
+            //i=4,时间:20
+            //i=5,时间:20
+            //i=6,时间:20
+            //i=7,时间:20
+            //i=8,时间:20
+            //i=9,时间:19
+            //avg:19.9
+            //IList---------------
+            //i=0,时间:33
+            //i=1,时间:33
+            //i=2,时间:33
+            //i=3,时间:32
+            //i=4,时间:33
+            //i=5,时间:32
+            //i=6,时间:32
+            //i=7,时间:31
+            //i=8,时间:31
+            //i=9,时间:31
+            //avg:32.1
+            //List---------------
+            //i=0,时间:10
+            //i=1,时间:9
+            //i=2,时间:10
+            //i=3,时间:9
+            //i=4,时间:9
+            //i=5,时间:10
+            //i=6,时间:10
+            //i=7,时间:10
+            //i=8,时间:10
+            //i=9,时间:10
+            //avg:9.7
 
-            Console.ReadKey();
+            //list dictionary array set queue
         }
 
         public static void IEnumerableForeachTest()
         {
+            long t1 = 0;
+            var array = GetArray();
             for (int i = 0; i < 10; i++)
             {
                 var watch = Stopwatch.StartNew();
-                foreach (var item in GetArray())
+                foreach (var item in array)
                 {
                     var a = item + 1;
                 }
                 watch.Stop();
+                t1 += watch.ElapsedMilliseconds;
                 Console.WriteLine($"i={i},时间:{watch.ElapsedMilliseconds}");
             }
+            Console.WriteLine($"avg:{t1 / 10.0}");
 
             Console.WriteLine("IEnumerable---------------");
+            long t2 = 0;
+            var iEnumerable = GetEnumerable();
             for (int i = 0; i < 10; i++)
             {
                 var watch = Stopwatch.StartNew();
-                foreach (var item in GetEnumerable())
+                foreach (var item in iEnumerable)
                 {
                     var a = item + 1;
                 }
                 watch.Stop();
+                t2 += watch.ElapsedMilliseconds;
                 Console.WriteLine($"i={i},时间:{watch.ElapsedMilliseconds}");
             }
+            Console.WriteLine($"avg:{t2 / 10.0}");
 
             int[] GetArray()
             {
                 return Enumerable.Range(1, 5_000_000).ToArray();
             }
-            
+
             IEnumerable<int> GetEnumerable()
             {
                 return Enumerable.Range(1, 5_000_000);
@@ -59,17 +124,21 @@ namespace ForeachTest
         public static void IListForeachTest()
         {
             Console.WriteLine("IList---------------");
+            long t2 = 0;
+            var iList = GetIList();
             for (int i = 0; i < 10; i++)
             {
                 var watch = Stopwatch.StartNew();
-                foreach (var item in GetIList())
+                foreach (var item in iList)
                 {
                     var a = item + 1;
                 }
                 watch.Stop();
+                t2 += watch.ElapsedMilliseconds;
                 Console.WriteLine($"i={i},时间:{watch.ElapsedMilliseconds}");
             }
-            
+            Console.WriteLine($"avg:{t2 / 10.0}");
+
             IList<int> GetIList()
             {
                 return Enumerable.Range(1, 5_000_000).ToList();
@@ -79,18 +148,22 @@ namespace ForeachTest
         public static void ListForeachTest()
         {
             Console.WriteLine("List---------------");
+            long t2 = 0;
+            var list = GetList();
             for (int i = 0; i < 10; i++)
             {
                 var watch = Stopwatch.StartNew();
-                foreach (var item in GetIList())
+                foreach (var item in list)
                 {
                     var a = item + 1;
                 }
                 watch.Stop();
+                t2 += watch.ElapsedMilliseconds;
                 Console.WriteLine($"i={i},时间:{watch.ElapsedMilliseconds}");
             }
-            
-            List<int> GetIList()
+            Console.WriteLine($"avg:{t2 / 10.0}");
+
+            List<int> GetList()
             {
                 return Enumerable.Range(1, 5_000_000).ToList();
             }
