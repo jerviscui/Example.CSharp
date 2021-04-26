6c87ee47f323b238aec6e0f4e07639eb217a3107
jerviscui <cuijervis@126.com>
2021-04-26 23:35:43 +0800

    update

--- src/9/TaskGuidanceTest/Program.cs
+++ src/9/TaskGuidanceTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace TaskGuidanceTest
     {
         static void Main(string[] args)
         {
-            var t = TaskTest.SimpleTask();
+            var t = TaskTest.SimpleTask2();
 
             var awaiter = t.GetAwaiter();
             awaiter.GetResult();//阻塞主线程
@@ -29,11 +30,32 @@ namespace TaskGuidanceTest
 
             public static async Task SimpleTask2()
             {
-                AsyncTaskMethodBuilder<int>
-
-                Console.WriteLine("SimpleTask2");
-                await Task.Delay(1000 * 30);
-                Console.WriteLine("Completed");
+                Console.WriteLine($"SimpleTask2 {Thread.CurrentThread.ManagedThreadId}");
+                await Task.Delay(1000).ContinueWith(_ => Console.WriteLine($"task {Thread.CurrentThread.ManagedThreadId}"));
+                var s = new StackTrace().GetFrames();
+                foreach (var stackFrame in s)
+                {
+                    Console.WriteLine(stackFrame.ToString());
+                }
+                Console.WriteLine($"Completed {Thread.CurrentThread.ManagedThreadId}");
+                //调用堆栈输出
+                //MoveNext at offset 754 in file: line: column < filename unknown >:0:0
+                //ExecutionContextCallback at offset 21 in file: line: column < filename unknown >:0:0
+                //RunInternal at offset 128 in file: line: column < filename unknown >:0:0
+                //MoveNext at offset 147 in file: line: column < filename unknown >:0:0
+                //MoveNext at offset 12 in file: line: column < filename unknown >:0:0
+                //<OutputWaitEtwEvents>b__12_0 at offset 266 in file: line: column < filename unknown >:0:0
+                //Invoke at offset 33 in file: line: column < filename unknown >:0:0
+                //RunOrScheduleAction at offset 106 in file: line: column < filename unknown >:0:0
+                //RunContinuations at offset 213 in file: line: column < filename unknown >:0:0
+                //FinishContinuations at offset 54 in file: line: column < filename unknown >:0:0
+                //FinishStageThree at offset 47 in file: line: column < filename unknown >:0:0
+                //FinishStageTwo at offset 384 in file: line: column < filename unknown >:0:0
+                //ExecuteWithThreadLocal at offset 516 in file: line: column < filename unknown >:0:0
+                //ExecuteEntryUnsafe at offset 83 in file: line: column < filename unknown >:0:0
+                //ExecuteFromThreadPool at offset 10 in file: line: column < filename unknown >:0:0
+                //Dispatch at offset 442 in file: line: column < filename unknown >:0:0
+                //PerformWaitCallback at offset 10 in file: line: column < filename unknown >:0:0
             }
 
             public static async Task<int> GenericTask()
