bbb524b773af9dbd301d8f68b284fb4e9e6bd8f6
jerviscui <cuijervis@126.com>
2021-07-14 18:27:10 +0800

    DictionaryTest

---
+++ src/Tests/DictionaryTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace DictionaryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////ToArray 快
+            //BenchmarkRunner.Run<ToArrayAndListTests>();
+
+            ////array 最快，IEnumerable IList 有装箱，
+            //BenchmarkRunner.Run<ForeachTest>();
+            
+            ////Test();
+        }
+    }
+}
