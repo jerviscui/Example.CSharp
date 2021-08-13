509862c6265c0a5874dd684b576b949d77962ace
jerviscui <cuijervis@126.com>
2021-08-13 17:30:34 +0800

    add EfCoreTest

---
+++ src/plugins/EfCoreTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
