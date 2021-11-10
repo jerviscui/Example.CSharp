5fb486983c066f38d319b38c40e7a4747d39167b
jerviscui <cuijervis@126.com>
2021-11-10 19:52:12 +0800

    Add AsyncLockTest

--- src/plugins/AsyncEx/AsyncExConsoleTest/AsyncLockTest.cs
+++ src/plugins/AsyncEx/AsyncExConsoleTest/AsyncLockTest.cs
@@ -45,13 +45,47 @@ namespace AsyncExConsoleTest
                     {
                         return count = count + 1;
                     }
-                });
+                }).Unwrap();
                 tasks[i] = task;
             }
 
             await tasks.WhenAll();
 
             Console.WriteLine($"result is {count}, must equals {tasks.Length}");
+        }
+
+        public static async Task AsyncLock_UsedAsync_WithMethod_Test()
+        {
+            var count = 0;
+            var asyncLock = new AsyncLock();
+
+            var tasks = new Task[10000];
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                var task = Fun();
+                tasks[i] = task;
+            }
+
+            await tasks.WhenAll();
+
+            Console.WriteLine($"result is {count}, must equals {tasks.Length}");
+
+            async Task<int> Fun()
+            {
+                using (await asyncLock.LockAsync())
+                {
+                    return count = count + 1;
+                }
+            }
+        }
+
+        public void Test()
+        {
+            var asyncReaderWriterLock = new AsyncReaderWriterLock();
+
+            asyncReaderWriterLock.ReaderLock();
+
+            asyncReaderWriterLock.WriterLock();
         }
     }
 }
