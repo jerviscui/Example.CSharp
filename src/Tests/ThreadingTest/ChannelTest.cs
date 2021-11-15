d885b89c48a013889998d6ede4a51b14add2b2ee
jerviscui <cuijervis@126.com>
2021-11-16 00:06:22 +0800

    Update ChannelTest

--- src/Tests/ThreadingTest/ChannelTest.cs
+++ src/Tests/ThreadingTest/ChannelTest.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Threading.Channels;
+using System.Threading.Tasks;
 
 namespace ThreadingTest
 {
     internal class ChannelTest
     {
-        public void Test()
-        {
-            var channel = Channel.CreateBounded<string>(new BoundedChannelOptions(100)
+        private static Channel<string>? _channel;
+
+        private static Channel<string> GetBoundedChannel() => _channel ??=
+            Channel.CreateBounded<string>(new BoundedChannelOptions(100)
             {
                 FullMode = BoundedChannelFullMode.Wait,
                 AllowSynchronousContinuations = false,
@@ -14,10 +17,40 @@ namespace ThreadingTest
                 SingleWriter = false
             });
 
-            channel = Channel.CreateUnbounded<string>(new UnboundedChannelOptions
+        private static Channel<string> GetUnbounded() => _channel ??=
+            Channel.CreateUnbounded<string>(new UnboundedChannelOptions
             {
                 AllowSynchronousContinuations = false, SingleReader = true, SingleWriter = false
             });
+
+        public static async Task Test()
+        {
+            var channel = GetBoundedChannel();
+            var writer = channel.Writer;
+
+            await writer.WriteAsync("");
+
+            while (await writer.WaitToWriteAsync())
+            {
+                if (writer.TryWrite(""))
+                {
+                    return;
+                }
+            }
+        }
+
+        public static async Task Read()
+        {
+            var channel = GetBoundedChannel();
+            var reader = channel.Reader;
+
+            while (await reader.WaitToReadAsync())
+            {
+                while (reader.TryRead(out var item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
