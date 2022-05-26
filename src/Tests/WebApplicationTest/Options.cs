928cbb340e9028f69d13658bd29a7f3648046982
jerviscui <cuijervis@126.com>
2022-05-26 21:01:28 +0800

    Update SimpleOptions

--- src/Tests/WebApplicationTest/Options.cs
+++ src/Tests/WebApplicationTest/Options.cs
@@ -3,6 +3,12 @@ namespace WebApplicationTest;
 public class SimpleOptions
 {
     public ICollection<string> Parkings { get; set; } = new List<string>();
+
+    public IEnumerable<string> InitArray { get; set; } = Array.Empty<string>(); //Wrong！无法通过配置文件赋值
+
+    public string[] InitArray2 { get; set; } = Array.Empty<string>();
+
+    public string[] InitArray3 { get; set; } = null!; //配置文件为空数组，属性会返回 null 而不是空数组
 }
 
 public class ReadOnlyOptions
