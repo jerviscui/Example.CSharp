185d1c38d939462a0d7c03be82c61a4fddba7085
jerviscui <cuijervis@126.com>
2023-04-15 19:43:11 +0800

    Update CustomConventions

--- src/plugins/RabbitMq/EasyNetQTest/CustomConventions.cs
+++ src/plugins/RabbitMq/EasyNetQTest/CustomConventions.cs
@@ -1,4 +1,5 @@
 using EasyNetQ;
+using EasyNetQ.Internals;
 
 namespace EasyNetQTest;
 
@@ -7,9 +8,18 @@ public class CustomConventions : Conventions
     /// <inheritdoc />
     public CustomConventions(ITypeNameSerializer typeNameSerializer) : base(typeNameSerializer)
     {
-        ExchangeNamingConvention = type => "ExEasyNetQTest";
+        ExchangeNamingConvention = type =>
+        {
+            var ex = GetQueueAttribute(type)?.ExchangeName;
+            return string.IsNullOrEmpty(ex) ? "ExEasyNetQTest" : ex;
+        };
 
         //Topic RoutingKey
         TopicNamingConvention = type => type.Name;
+    }
+
+    private static QueueAttribute? GetQueueAttribute(Type messageType)
+    {
+        return messageType.GetAttribute<QueueAttribute>();
     }
 }
