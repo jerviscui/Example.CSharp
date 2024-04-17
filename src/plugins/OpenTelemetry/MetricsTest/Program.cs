de2f0b47b20ccb041234fe5d2b8ee7f5d08e31aa
jerviscui <cuijervis@126.com>
2024-04-17 18:57:17 +0800

    Update

--- src/plugins/OpenTelemetry/MetricsTest/Program.cs
+++ src/plugins/OpenTelemetry/MetricsTest/Program.cs
@@ -1,2 +1,32 @@
+using System.Diagnostics.Metrics;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
 
 Console.WriteLine("Hello, World!");
+
+var meter = new Meter("Sample.Meter", "1.0.0");
+var counter = meter.CreateCounter<int>("sales-counter");
+
+var builder = Sdk.CreateMeterProviderBuilder();
+builder.AddMeter(meter.Name);
+//builder.AddPrometheusHttpListener(options =>
+//{
+//    //http: //localhost:9464/metrics
+//    options.UriPrefixes = new[] { "http://localhost:9464/" };
+//});
+builder.AddOtlpExporter((exporterOptions, metricReaderOptions) =>
+{
+    exporterOptions.Endpoint = new Uri("http://localhost:4318/api/v1/otlp/v1/metrics");
+    exporterOptions.Protocol = OtlpExportProtocol.HttpProtobuf;
+    metricReaderOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 1000;
+});
+
+using var meterProvider = builder.Build();
+
+var random = Random.Shared;
+while (!Console.KeyAvailable)
+{
+    await Task.Delay(500);
+    counter.Add(random.Next(10));
+}
