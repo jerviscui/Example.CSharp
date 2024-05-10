e4f751d0abcf6d4401ed10b29498b63da7984a7b
jerviscui <cuijervis@126.com>
2024-05-10 18:37:31 +0800

    Update

--- src/Tests/CodeAnalysisTest/PatternMatchingTest.cs
+++ src/Tests/CodeAnalysisTest/PatternMatchingTest.cs
@@ -4,7 +4,7 @@ namespace CodeAnalysisTest;
 
 internal sealed class PatternMatchingTest
 {
-    private static void M(object o)
+    private static int M(object o)
     {
         // ide0019 csharp_style_pattern_matching_over_as_with_null_check = true
         var s = o as string;
@@ -15,5 +15,39 @@ internal sealed class PatternMatchingTest
 
         // IDE0038 csharp_style_pattern_matching_over_is_with_cast_check = true
         if (o is int) { Console.WriteLine((int)o); }
+
+        // ide0066 csharp_style_prefer_switch_expression = true
+        int x = Random.Shared.Next();
+        int i;
+        switch (x)
+        {
+            case 1:
+                i = 1 * 1;
+                break;
+            case 2:
+                i = 2 * 2;
+                break;
+            default:
+                i = 0;
+                break;
+        }
+
+        // IDE0078 csharp_style_prefer_pattern_matching = false
+        //var b = !(o is PatternMatchingTest p);
+
+        // IDE0260 csharp_style_pattern_matching_over_as_with_null_check = true
+        if ((o as string)?.Length == 0)
+        {
+            return 1;
+        }
+
+        return 0;
     }
+
+    public record Point(int X, int Y);
+    public record Segment(Point Start, Point End);
+
+    // ide0170 Simplify property pattern
+    static bool IsEndOnXAxis(Segment segment) =>
+        segment is { Start: { Y: 0 } } or { End: { Y: 0 } };
 }
