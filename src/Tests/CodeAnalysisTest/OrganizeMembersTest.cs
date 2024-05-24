57c0b984ee8191fba7952799926a47f70eb59205
jerviscui <cuijervis@126.com>
2024-05-24 19:27:53 +0800

    Update

---
+++ src/Tests/CodeAnalysisTest/OrganizeMembersTest.cs
@@ -0,0 +1,41 @@
+﻿namespace CodeAnalysisTest;
+
+public class OrganizeMembersTest
+{
+    // IDE0001 dotnet_style_predefined_type_for_locals_parameters_members = true
+    private int _id;
+
+    // IDE0001 dotnet_style_predefined_type_for_locals_parameters_members = true
+    public int ID
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    // IDE0001 dotnet_style_predefined_type_for_locals_parameters_members = true
+    public void Display()
+    {
+        throw new NotImplementedException();
+    }
+
+    // IDE0001 dotnet_style_predefined_type_for_locals_parameters_members = true
+    public event EventHandler MyEvent;
+
+    // IDE0001 dotnet_style_predefined_type_for_locals_parameters_members = true
+    private static bool Test()
+    {
+        // ide0007 csharp_style_var_for_built_in_types = true
+        int x = 5;
+        int y = x;
+
+        // ide0007 csharp_style_var_when_type_is_apparent = true
+        MyClass obj = new MyClass();
+        var yy = obj.Age;
+
+        // ide0007 csharp_style_var_elsewhere = true
+        bool f = Random.Shared.Next() > 1;
+        var yyy = f;
+
+        return true;
+    }
+}
