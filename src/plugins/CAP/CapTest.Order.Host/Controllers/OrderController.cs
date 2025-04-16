fe0e17d5509cf1fccfe977cd265d4bbdd63a58f2
jerviscui <cuijervis@126.com>
2025-04-17 00:10:27 +0800

    Update

--- src/plugins/CAP/CapTest.Order.Host/Controllers/OrderController.cs
+++ src/plugins/CAP/CapTest.Order.Host/Controllers/OrderController.cs
@@ -1,42 +1,45 @@
-using System.Threading.Tasks;
 using CapTest.Order.Service;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CapTest.Order.Host.Controllers
+namespace CapTest.Order.Host.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class OrderController : ControllerBase
+    private readonly OrderService _orderService;
+
+    public OrderController(OrderService orderService)
     {
-        private readonly OrderService _orderService;
-
-        public OrderController(OrderService orderService)
-        {
-            _orderService = orderService;
-        }
-
-        [HttpPost]
-        [Route("{id}")]
-        public async Task<string> CreateAsync(int id)
-        {
-            return await _orderService.Create(id);
-        }
-
-        [HttpPost]
-        [Route("CreateWithTransaction/{id}")]
-        public async Task<string> CreateWithTransactionAsync(int id)
-        {
-            return await _orderService.CreateWithoutCapPgsql(id);
-        }
-
-        [HttpPost]
-        [Route("CreateMessageWithHeaders")]
-        // ReSharper disable once InconsistentNaming
-        public async Task<IActionResult> CreateTTLMessage()
-        {
-            await _orderService.CreateMessageWithHeaders();
-
-            return Ok();
-        }
+        _orderService = orderService;
     }
+
+    #region Methods
+
+    [HttpPost]
+    [Route("{id}")]
+    public Task<string> CreateAsync(int id)
+    {
+        return _orderService.Create(id);
+    }
+
+    [HttpPost]
+    [Route("CreateMessageWithHeaders")]
+    // ReSharper disable once InconsistentNaming
+    public async Task<IActionResult> CreateTTLMessage()
+    {
+        await _orderService.CreateMessageWithHeaders();
+
+        return Ok();
+    }
+
+    [HttpPost]
+    [Route("CreateWithTransaction/{id}")]
+    public Task<string> CreateWithTransactionAsync(int id)
+    {
+        return _orderService.CreateWithoutCapPgsql(id);
+    }
+
+    #endregion
+
 }
