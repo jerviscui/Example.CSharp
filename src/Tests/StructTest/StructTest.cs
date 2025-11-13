9c9c4ac85d921c6c4197cf258cd7b37aa06d8f4f
jerviscui <cuijervis@126.com>
2025-11-13 13:34:55 +0800

    Update

--- src/Tests/StructTest/StructTest.cs
+++ src/Tests/StructTest/StructTest.cs
@@ -1,7 +1,5 @@
 using ObjectLayoutInspector;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace StructTest;
@@ -10,6 +8,12 @@ internal static class StructTest
 {
 
     #region Constants & Statics
+
+    public static void Complex_Test()
+    {
+        var r = new Result<BaseError>(new BaseError(11, null, null));
+        TypeLayout.PrintLayout(r.GetType(), true);
+    }
 
     public static void Simple_Test()
     {
@@ -66,11 +70,6 @@ internal static class StructTest
 
     #endregion
 
-    public static void Complex_Test()
-    {
-        var r = new Result<BaseError>(new BaseError());
-        TypeLayout.PrintLayout(r.GetType(), true);
-    }
 }
 
 #region Simple Structs
@@ -108,36 +107,18 @@ public readonly record struct InnerStruct
 
 #region Complex
 
-//[StructLayout(LayoutKind.Auto, Pack = 1)]
-[StructLayout(LayoutKind.Sequential, Pack = 1)]
+[StructLayout(LayoutKind.Auto, Pack = 1)]
+//[StructLayout(LayoutKind.Sequential, Pack = 1)]
 [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
 public readonly record struct Result<TError>
     where TError : struct
 {
-
-    #region Constants & Statics
-
-    /// <summary>
-    /// No errors, just return.
-    /// </summary>
-    private static readonly Result<TError> Ok = new(true);
-
-    #endregion
-
     /// <summary>
     /// Gets the error.
     /// </summary>
     internal readonly TError _error;
 
     internal readonly bool _hasError;
-
-#pragma warning disable IDE0060 // Remove unused parameter
-    private Result(bool isOk)
-#pragma warning restore IDE0060 // Remove unused parameter
-    {
-        // just use for Ok
-        _hasError = false;
-    }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Result{TError}"/> with default <typeparamref name="TError"/>.
@@ -156,168 +137,30 @@ public readonly record struct Result<TError>
         _error = error;
         _hasError = true;
     }
-
-    #region Methods
-
-    [UnscopedRef]
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal ref readonly TError UnwrapErrorWithoutCheck()
-    {
-        Debug.Assert(_hasError, $"{nameof(_hasError)} is true");
-        return ref _error;
-    }
-
-    /// <summary>
-    /// Determines whether this instance is error.
-    /// </summary>
-    /// <returns>
-    /// <c>true</c> if this instance is error; otherwise <c>false</c>.
-    /// </returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public readonly bool IsError()
-    {
-        return _hasError;
-    }
-
-    /// <summary>
-    /// Determines whether this instance is error.
-    /// </summary>
-    /// <returns>
-    /// <c>true</c> if this instance is error, and error must not be null; otherwise, <c>false</c>.
-    /// </returns>
-    [SuppressMessage(
-        "Critical Code Smell",
-        "S3874:\"out\" and \"ref\" parameters should not be used",
-        Justification = "<Pending>")]
-    public readonly bool IsError([NotNullWhen(true)] out TError? error)
-    {
-        error = null;
-
-        if (_hasError)
-        {
-            error = _error;
-            return true;
-        }
-
-        return false;
-    }
-
-    #endregion
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static implicit operator Result<TError>(in TError error) => new(in error);
 }
 
-//[StructLayout(LayoutKind.Auto, Pack = 1)]
-[StructLayout(LayoutKind.Sequential, Pack = 1)]
-public readonly record struct BaseError(BaseErrorCode Code, string? Reason = null/*, Exception? Exception = null*/)
-//: IBaseError<BaseError, BaseErrorCode>
+[StructLayout(LayoutKind.Auto, Pack = 1)]
+//[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public readonly record struct BaseError
 {
-    public BaseError() : this(BaseErrorCode.Failed)
-    {
-    }
-
-    #region IBaseError implementations
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Result<BaseError> Result(BaseErrorCode code, string? reason = null, Exception? exception = null)
-    {
-        return new BaseError(code, reason/*, exception*/);
-    }
-
-    #endregion
-
-    #region IError implementations
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Result<BaseError> Result()
-    {
-        return new BaseError();
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Result<BaseError> Result(BaseErrorCode code)
-    {
-        return new BaseError(code);
-    }
-
-    #endregion
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static implicit operator BaseError(BaseErrorCode errorCode) => new(errorCode);
-}
-
-public interface IError<TError, TCode>
-    where TError : struct
-{
-
-    #region Constants & Statics
-
-    /// <summary>
-    /// Create a default result.
-    /// </summary>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static abstract Result<TError> Result();
-
-    /// <summary>
-    /// Create a result with the specified code.
-    /// </summary>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static abstract Result<TError> Result(TCode code);
-
-    #endregion
-
-    #region Properties
-
-    /// <summary>
-    /// Gets the error code.
-    /// </summary>
-    public TCode Code { get; }
-
-    #endregion
-
-}
-
-public interface IBaseError<TError, TCode> : IError<TError, TCode>
-    where TError : struct
-{
-
-    #region Constants & Statics
-
-    /// <summary>
-    /// Create a result with the specified code, reason and exception.
-    /// </summary>
-    /// <param name="code">The code.</param>
-    /// <param name="reason">The reason.</param>
-    /// <param name="exception">The exception.</param>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static abstract Result<TError> Result(TCode code, string? reason = null, Exception? exception = null);
-
-    #endregion
-
-    #region Properties
+    public readonly int Code;
 
     /// <summary>
     /// Gets the exception.
     /// </summary>
-    public Exception? Exception { get; }
+    public readonly Exception? Exception;
 
     /// <summary>
     /// Gets the reason.
     /// </summary>
-    public string? Reason { get; }
+    public readonly string? Reason;
 
-    #endregion
-
-}
-
-public enum BaseErrorCode
-{
-    Failure = 0,
-
-    Failed = 10,
-
-    NotFound = 99
+    public BaseError(int code, Exception? exception, string? reason)
+    {
+        Code = code;
+        //Exception = exception;
+        Reason = reason;
+    }
 }
 
 #endregion
