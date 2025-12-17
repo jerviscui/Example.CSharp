d7639d87ce794e641d757a44642382aa765a9473
jerviscui <cuijervis@126.com>
2025-12-17 20:40:57 +0800

    Add metrics config

--- src/plugins/NewtonsoftJsonTest/Program.cs
+++ src/plugins/NewtonsoftJsonTest/Program.cs
@@ -24,7 +24,7 @@ internal sealed class Program
         //SystemTextJsonTestGeneratorTest.SerializeTest();
         //SystemTextJsonTestGeneratorTest.DeserializeTest();
 
-        //BenchmarkRunner.Run<SystemTextJsonBenchmarks>();
+        //BenchmarkRunner.Run<SystemTextJsonBenchmarks>();        
     }
 
     #endregion
