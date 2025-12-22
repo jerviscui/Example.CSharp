49c9418e5a34600cb1a3618fa705b65dfe2d905a
jerviscui <cuijervis@126.com>
2025-12-22 18:04:44 +0800

    Add MapperlyGlobalConfig

--- src/plugins/MapperlyTest/CarMapper.cs
+++ src/plugins/MapperlyTest/CarMapper.cs
@@ -1,13 +1,5 @@
 using Riok.Mapperly.Abstractions;
 
-[assembly: MapperDefaults(
-    AllowNullPropertyAssignment = true,
-    ThrowOnPropertyMappingNullMismatch = true,
-    ThrowOnMappingNullMismatch = true,
-    RequiredMappingStrategy = RequiredMappingStrategy.None,
-    AutoUserMappings = false,
-    IgnoreObsoleteMembersStrategy = IgnoreObsoleteMembersStrategy.None)]
-
 namespace MapperlyTest;
 
 public class Car
---
+++ src/plugins/MapperlyTest/MapperlyGlobalConfig.cs
@@ -0,0 +1,9 @@
+using Riok.Mapperly.Abstractions;
+
+[assembly: MapperDefaults(
+    AllowNullPropertyAssignment = true,
+    ThrowOnPropertyMappingNullMismatch = true,
+    ThrowOnMappingNullMismatch = true,
+    RequiredMappingStrategy = RequiredMappingStrategy.None,
+    AutoUserMappings = false,
+    IgnoreObsoleteMembersStrategy = IgnoreObsoleteMembersStrategy.None)]
